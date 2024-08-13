e1e7923f2e475f2412d86a7ae4ed39a492dc8ab7
Italo Reis <italohenrikbr@gmail.com>
2024-08-13 10:03:24 -0300

    current

---
+++ Heaps/max-heapify/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int left(int i) {
+    return 2*i;
+}
+
+int right(int i) {
+    return 2*i + 1;
+}
+
+void max_heapify(int *array, int i, int size) {
+    int l = left(i);
+    int r = right(i);
+    int largest;
+
+    if (l < size && array[l] > array[i])
+        largest = l;
+    else
+        largest = i;
+
+    if (r <= size && array[r] > array[largest]) 
+        largest = r;
+    
+    if (largest != i) {
+        int aux = array[i];
+        array[i] = array[largest];
+        array[largest] = aux;
+        max_heapify(array, largest, size);
+    }
+}
+
+int main() {
+
+    int array[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};   
+
+    int size1 = sizeof(array) / sizeof(array[0]);   //tamanho do array
+    printf("array size -> %d\n", size1);
+    max_heapify(array, 1, size1);
+    for(int i = 0; i < size1; i++) 
+        printf("%d, ", array[i]);
+    
+    return 0;
+}
